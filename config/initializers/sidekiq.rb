e8d6e32c320ba04e387fd4c0b6b8560fa190721e
Giovanni Estrada <giovanni.estrada@gurucomm.mx>
2024-10-19 16:03:25 -0600

    fix deploy

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,6 +13,7 @@ Sidekiq.configure_server do |config|
   config.redis = { url: sidekiq_redis }
   config.logger.level = Logger::ERROR
 end
+
 Sidekiq.configure_client do |config|
   config.redis = { url: sidekiq_redis }
   config.logger.level = Logger::ERROR
@@ -24,4 +25,10 @@ return unless Rails.env.production? || Rails.env.stage?
 file_path = 'config/schedule.yml'
 return unless File.exist?(file_path)
 
-Sidekiq::Cron::Job.load_from_hash YAML.load_file(file_path)
+# Manejo seguro de YAML
+begin
+  schedule = YAML.safe_load(File.read(file_path), permitted_classes: [Symbol])
+  Sidekiq::Cron::Job.load_from_hash(schedule) if schedule.present?
+rescue Psych::SyntaxError => e
+  Rails.logger.error "Error loading Sidekiq schedule: #{e.message}"
+end
